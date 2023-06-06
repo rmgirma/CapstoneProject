0ab171cfa3168e5e332c6434f96c29c6d6ca63a0
Robel Girma <rmgirma@hotmail.com>
2023-06-06 18:48:49 -0400

    Added function sum_by_type to display the count and total value for a given Type

--- creditcard_capstone.py
+++ creditcard_capstone.py
@@ -21,7 +21,7 @@ cursor = conn.cursor()
 nav = "Press Enter key to continue..." # navigation prompt
 padding = 65 # length of character for padding 
 
-# function to display transactions by zipcode
+# function to display transactions by zipcode for Question 2.1
 def zipcode_transactions():
     try:  # Start of try block
         clear_screen()
@@ -31,81 +31,82 @@ def zipcode_transactions():
         zipcode = input("Enter Zip Code: ")
         month = input("Enter Month as 2 digits: ")
         year = input("Enter Year as YYYY: ")
-        query = """SELECT ccard.TIMEID, cust.FIRST_NAME, cust.LAST_NAME, cust_phone, cust.cust_zip, 
-                cust.CREDIT_CARD_NO, ccard.TRANSACTION_TYPE, ccard.TRANSACTION_VALUE
+        query = """SELECT ccard.TIMEID, cust.FIRST_NAME, cust.LAST_NAME, cust.cust_phone, cust.FULL_STREET_ADDRESS,
+                cust.cust_zip, cust.CREDIT_CARD_NO, ccard.TRANSACTION_TYPE, ccard.TRANSACTION_VALUE
                 FROM cdw_sapp_customer cust join cdw_sapp_credit_card ccard on ccard.CUST_SSN = cust.SSN
                 where cust.CUST_ZIP =  %s and MONTH(ccard.TIMEID) = %s AND YEAR(ccard.TIMEID) = %s
                 order by ccard.TIMEID desc"""
         cursor.execute(query, (zipcode, month, year))
         results = cursor.fetchall()
         pretty = PrettyTable()  # create a PrettyTable object to display result in tabular manner
-        pretty.field_names = ['Date', 'First Name', 'Last Name', 'Phone Number', 'Zip Code', 'Credit Card Number', 'Transaction Type', 'Transaction Date']
+        pretty.field_names = ['Date', 'First Name', 'Last Name', 'Phone Number', 'Street Address', 'Zip Code', 'Credit Card Number', 'Transaction Type', 'Amount']
         for result in results:
             pretty.add_row(result)
         print(pretty)
         input(nav)
     except Exception as err:  # Catch and display errors here
+        print(err)
+
+def sum_by_type():  # display the number and total value for a given Type
+    try:
+        clear_screen()
+        while True:
+            print("\n" + "=" * padding)
+            print("Select Transaction Type".center(60))
+            print("=" * padding)
+            print("     1. Education")
+            print("     2. Entertainment")
+            print("     3. Healthcare")
+            print("     4. Grocery")
+            print("     5. Test")
+            print("     6. Gas")
+            print("     7. Bills")
+            print("=" * padding)
+            print("     8. Return to Main Menu")
+            print("=" * padding)
+            choice = input("Select Transaction Type: ")
+            ttype =''
+            if choice == '1':
+                ttype ='Education'
+            elif choice == '2':
+                ttype = 'Entertainment'
+            elif choice == '3':
+                ttype = 'Healthcare'
+            elif choice == '4':
+                ttype = 'Grocery'
+            elif choice == '5':
+                ttype = 'Test'
+            elif choice == '6':
+                ttype = 'Gas'
+            elif choice == '7':
+                ttype = 'Bills'  
+            elif choice == '8':
+                print("Returning to Main MENU...")     
+                break
+            else:
+                print("Invalid choice. Please enter a valid Transaction Type.")
+            query = """SELECT TRANSACTION_TYPE,  count(TRANSACTION_TYPE) total_num, sum(TRANSACTION_VALUE) tot_amount from CDW_SAPP_CREDIT_CARD
+                            group by TRANSACTION_TYPE
+                            having TRANSACTION_TYPE= %s """
+            cursor.execute(query, (ttype,))
+            results = cursor.fetchall()
+            pretty = PrettyTable()  # create a PrettyTable object to display result in tabular manner
+            pretty.field_names = ['Transaction Type', 'Total Number', 'Total Amount']
+            for result in results:
+                pretty.add_row(result)
+            print(pretty)
+            input(nav)
+            clear_screen()
+    except Exception as err:
         print(f"An error occurred: {err}")
 
-def sum_by_type():
-    clear_screen()
-    while True:
-        print("\n" + "=" * padding)
-        print("Select Transaction Type".center(60))
-        print("=" * padding)
-        print("     1. Education")
-        print("     2. Entertainment")
-        print("     3. Healthcare")
-        print("     4. Grocery")
-        print("     5. Test")
-        print("     6. Gas")
-        print("     7. Bills")
-        print("=" * padding)
-        print("     8. Return to Main Menu")
-        print("=" * padding)
-        choice = input("Select Transaction Type: ")
-        if choice == '1':
-            print('Education')
-            input(nav)
-            clear_screen()
-        elif choice == '2':
-            print('Entertainment')
-            input(nav)
-            clear_screen()
-        elif choice == '3':
-            print('Healthcare')  
-            input(nav)
-            clear_screen()
-        elif choice == '4':
-            print('Grocery')
-            input(nav)
-            clear_screen()
-        elif choice == '5':
-            print('Test')
-            input(nav)
-            clear_screen()
-        elif choice == '6':
-            print('Gas')
-            input(nav)
-            clear_screen()
-        elif choice == '7':
-            print('Bills') 
-            input(nav)
-            clear_screen()  
-        elif choice == '8':
-            print("Returning to Main MENU...")     
-            break
-        else:
-            print("Invalid choice. Please enter a valid Transaction Type.")
-
- 
 # Main Menu function        
 def main_menu():
     while True:
         clear_screen()
         print("\n" + "=" * padding)
         print("Loan Application Console Menu".center(60))
-        print("=" * 60)
+        print("=" * padding)
         print("\nPlease make your selection and press ENTER:")
         print(" ")
         print("     1. Display transactions for a given zip code")
